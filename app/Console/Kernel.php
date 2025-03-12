844636a122891a40ebaf1c924ddff75c7831322c
Andrew Master <code70@inbox.ru>
2025-03-13 02:40:26 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         //
-        $schedule->command('')->dailyAt('01:00');
     }
 
     /**
