034f0a786124faa62cc3717d899d0de6678513ab
Andrew Master <code70@inbox.ru>
2025-03-19 21:36:12 +0700

    add review page

--- resources/views/components/review-card.blade.php
+++ resources/views/components/review-card.blade.php
@@ -21,6 +21,15 @@
         <span class="font-semibold">Услуга:</span>
         <span itemprop="itemReviewed" itemscope itemtype="https://schema.org/Product">
             <span itemprop="name">{{ $review->service->name }}</span>
+            <span itemprop="aggregateRating" itemscope itemtype="https://schema.org/AggregateRating">
+                <meta itemprop="ratingValue" content="5">
+                <meta itemprop="reviewCount" content="{{ $review->service->reviews->count() }}">
+            </span>
+
+            <div itemprop="offers" itemscope itemtype="https://schema.org/Offer">
+                <meta itemprop="priceCurrency" content="RUB">
+                <meta itemprop="price" content="{{ $review->service->price }}">
+            </div>
         </span>
     </div>
 
@@ -28,13 +37,5 @@
         "{{ mb_strimwidth($review->message, 0, 100, '...') }}"
     </blockquote>
 
-    <span itemprop="aggregateRating" itemscope itemtype="https://schema.org/AggregateRating">
-        <meta itemprop="ratingValue" content="5">
-        <meta itemprop="reviewCount" content="{{ $review->service->reviews->count() }}">
-    </span>
 
-    <div itemprop="offers" itemscope itemtype="https://schema.org/Offer">
-        <meta itemprop="priceCurrency" content="RUB">
-        <meta itemprop="price" content="{{ $review->service->price }}">
-    </div>
 </div>
