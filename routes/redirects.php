5aee8758fddba696892fc64c9753c11d69b08025
Andrew Master <code70@inbox.ru>
2025-04-07 02:26:18 +0700

    add redirects routes group

--- routes/redirects.php
+++ routes/redirects.php
@@ -19,5 +19,5 @@ Route::redirect('/ustanovka-i-zamena-cirkuliacionnogo-nasosa', 'https://sales-to
 Route::redirect('/ustanovka-gruppy-bezopasnosti', 'https://sales-tomsk.ru/otoplenie-i-teplosnabzenie/ustanovka-i-zamena-gruppy-bezopasnosti', 301);
 Route::redirect('/obvyazka-teploobmennika-banya', 'https://sales-tomsk.ru/otoplenie-i-teplosnabzenie/obviazka-teploobmennika-v-bane', 301);
 Route::redirect('/shtroblenie-pod-truby', 'https://sales-tomsk.ru/dopolnitelnye-raboty/stroblenie-sten-pod-truby', 301);
-Route::redirect('/ustanovka-i-zamena-smesitelia', 'https://sales-tomsk.ru/ustanovka-santexniki/ustanovka-i-zamena-smesitelia', 301);
 Route::redirect('/ustanovka-vodonagrevatelya', 'https://sales-tomsk.ru/ustanovka-santexniki/ustanovka-vodonagrevatelia', 301);
+Route::redirect('/ustanovka-i-zamena-smesitelia', 'https://sales-tomsk.ru/ustanovka-santexniki/ustanovka-i-zamena-smesitelia', 301);
