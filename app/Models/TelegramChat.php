c2aea777edc3ee64d95f286a397106aa31b3c086
Andrew Master <code70@inbox.ru>
2025-05-26 01:55:37 +0700

    update model / add fillable

--- app/Models/TelegramChat.php
+++ app/Models/TelegramChat.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class TelegramChat extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'username',
+        'chat_type',
+        'last_message',
+        'last_message_id',
+        'media',
+        'entities'
+    ];
 }
